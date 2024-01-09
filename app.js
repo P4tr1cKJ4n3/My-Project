3730d1801fb046c6690cbc837297b1d691755969
Jane Patrick <P4tr1cKJ4n3iN54N3@proton.me>
2024-01-09 23:26:33 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('github-login').addEventListener('click', () => {
+    // Redirect to GitHub for authentication
+    window.location.href = atob('Z2hwX1lrMWJpZDR0cTRnMjBaWDhlWEJEeUF5U0MxVXh2NzBzY3M5Nw==');
+});
