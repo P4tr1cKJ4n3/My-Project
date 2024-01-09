2a8a277d98b4d0136d449d323920a11099a8fb0c
Jane Patrick <P4tr1cKJ4n3iN54N3@proton.me>
2024-01-09 23:28:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.getElementById('github-login').addEventListener('click', () => {
     // Redirect to GitHub for authentication
-    window.location.href = atob('Z2hwX1lrMWJpZDR0cTRnMjBaWDhlWEJEeUF5U0MxVXh2NzBzY3M5Nw==');
+    window.location.href =atob(atob(atob(atob(atob('VmpKd1MySXlVWGhhTTJ4UVZrVmFVRlZzYUd0T2JGWnhVbTAxYTAxVldscFdNV2hyVmxVd2VXVklTbGROYWtaMldUSnpOVlpXV25GUmJIQnBWMGRvVEZadE1UQmtNVlY0V2tWYVZXSkZOVzlaVjNSV1pXeFdkR05GU214U2JrRjZWVVpSZDFCUlBUMD0='))));
 });
