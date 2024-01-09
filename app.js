c132da63c1b60fc504ef66ff20255c45d82dba16
P4tr1cKJ4n3 <P4tr1cKJ4n3iN54N3@proton.me>
2024-01-09 23:03:48 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('github-login').addEventListener('click', () => {
-    // Redirect to GitHub for authentication
-    window.location.href = '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';
-});
