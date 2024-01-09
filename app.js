dbc6464473f4ef0bfd58c55f75fee8a4b1b4b841
Jane Patrick <P4tr1cKJ4n3iN54N3@proton.me>
2024-01-09 23:23:00 +0200

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('github-login').addEventListener('click', () => {
-    // Redirect to GitHub for authentication
-    window.location.href = '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';
-});
